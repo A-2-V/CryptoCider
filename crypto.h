c2050928c5b071f60e4dfa71c4e488e4d8a0a92c
A-2-V <177561919+A-2-V@users.noreply.github.com>
2024-08-14 01:17:46 +0700

    Наброски функций шифровальщика

---
+++ crypto.h
@@ -0,0 +1,10 @@
+/**КОНСТАНТЫ**/
+/* Размер строки с паорлем */
+#define PASSWORD_SIZE_STD 33
+
+#define A2V_str_standart_size 256
+
+extern struct st_sample_password_info {
+    char * p_ch_password;
+    int i_password_length;
+};
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <crypto.h>
+
+#define CHANGE_CODEPAGE system("chcp 1251")
+
+int main(int argc, char * argv[])
+{
+    CHANGE_CODEPAGE;
+
+    struct st_sample_password_info * p_password = f_st_pass_create();
+
+    printf("%d: %s\n", p_password->i_password_length, p_password->p_ch_password);
+
+
+    return 0;
+}
